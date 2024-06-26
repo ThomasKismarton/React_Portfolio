23897f4beef04d95ecdf3a11c21d745bd816d520
unknown <thomaskismarton@gmail.com>
2024-06-26 10:00:02 -0700

    Removing full address

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,13 +17,13 @@ export default function Contact() {
               src="https://www.google.com/maps/embed/v1/place?q=Redmond,+WA,+USA&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
             />
             <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-              <div className="lg:w-1/2 px-6">
+              <div className="lg:w-1/3 px-6">
                 <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                   ADDRESS
                 </h2>
                 <p className="mt-1">
-                  14408 NE 42nd Pl, Apt N903 <br />
-                  Bellevue, WA, 98007
+                  Based out of: <br />
+                  Redmond, WA 98007
                 </p>
               </div>
               <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
